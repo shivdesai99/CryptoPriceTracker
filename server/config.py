2c84fe14c0074ed23189dd58e4697122741f5e0c
Shiv Desai <shiv.desai99@gmail.com>
2025-02-21 14:13:41 -0500

    debugging db initialization

--- server/config.py
+++ server/config.py
@@ -3,9 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_FILE = os.getenv("DATABASE_FILE", "sqlite:///default.db")
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default_key")
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DATABASE_FILE}"
-    SQLALCHEMY_TRACK_MODIFICATONS = False
-    COINGECKO_API_KKEY = os.getenv("COINGECKO_API_KEY", "")
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.getenv('DATABASE_FILE', 'default.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    COINGECKO_API_KEY = os.getenv("COINGECKO_API_KEY", "")
